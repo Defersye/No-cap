b608e356e8e4b8a0fab667766ca8bf42cd0d11bf
Danil Burlakov <1321_budaan_nap@gagpk.ru>
2024-01-29 11:41:48 +0700

    add retropick content
    for a while, in next commits i must change whole content with brand new

--- views/HomeView.php
+++ views/HomeView.php
@@ -12,17 +12,21 @@ class HomeView
 
       <head>
          <meta charset="UTF-8">
-         <link rel="stylesheet" href="../public/css/style.css">
-         <title>Главная страница</title>
+         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+         <title>Retro Pick | online store for the sale of retro products</title>
+
+         <link rel="shortcut icon" href="./assets/img/layout/favicon.ico" type="image/x-icon">
+         <link rel="stylesheet" href="assets/css/general.css">
+         <link rel="stylesheet" href="assets/css/home.css">
       </head>
 
       <body>
          <?php
          include "./templates/header.html";
-         $this->catalog();
+         $this->home();
          include "./templates/footer.html";
          ?>
-         //Тут можно подключить js
       </body>
 
       </html>
@@ -32,5 +36,10 @@ class HomeView
    function catalog()
    {
       include "./views/catalog.php";
+   }
+
+   function home()
+   {
+      include "./views/home.php";
    }
 }
---
+++ views/home.php
@@ -0,0 +1,58 @@
+<section class="hero">
+   <div class="container">
+      <div class="hero_content">
+         <h1 class="hero_title">retro pick</h1>
+         <h2 class="hero_slogan">go get them lil collector</h2>
+         <h2 class="hero_description">Online store for the sale of retro equipment and accessories</h2>
+      </div>
+      <div class="hero_card">
+         <img src="assets/img/layout/hero_img.jpg" alt="cameras" class="hero_img">
+         <div class="hero_card_content">
+            <h6 class="hero_card_title">best antique for real</h6>
+            <p class="hero_card_p">Lorem iporem ipsum dolor sit amet. Dem autem incidunt sint, necessitatibus tempora cumque aperiam! Ducimus.</p>
+            <a href="catalog.php" class="hero_card_btn">show more</a>
+         </div>
+         </img>
+      </div>
+</section>
+<hr class="hero_hr">
+<section class="about">
+   <div class="container">
+      <div class="about_info">
+         <h2 class="about_title">about us</h2>
+         <div class="about_card"><img src="assets/img/layout/about_img.jpg" alt="" class="about_img"></div>
+      </div>
+      <p class="about_p"><span>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</span> <br>Reiciendis, voluptates quos! Quam distinctio ea fugiat repellendus magni blanditiis mollitia nam, repudiandae recusandae et expedita quis odit omnis nobis id officiis ipsum ut? Eius officia doloribus possimus expedita, <span>aliquam modi quibusdam dolorum magni natus</span>. <br><span>A iusto repellendus</span>, dolor quia nemo quaerat! Cupiditate maiores numquam nemo doloribus quam quisquam impedit, veritatis repellat magni nobis repudiandae accusamus culpa corporis temporibus. Quos nihil nobis possimus fuga alias tenetur modi magnam iste atque autem. Totam mollitia, quisquam eius neque a quis dolores illo reprehenderit doloribus veniam soluta cupiditate nam quam animi possimus enim iste labore odit distinctio expedita consequuntur quod tempora id. <br><span>Doloremque dignissimos id animi laborum</span>, eos sed hic voluptates commodi qui porro. Cupiditate!</p>
+      <div class="about_content">
+         <div class="about_col">
+            <h4 class="col_subtitle">customers'</h4>
+            <h3 class="col_title">trust</h3>
+            <img src="assets/img/layout/about_col_img-1.png" alt="" class="col_img">
+            <p class="col_p">Provide detailed product descriptions, including accurate specifications and high-quality images. Clearly outline pricing, shipping policies, and return procedures. Transparency instills confidence in customers, reassuring them of their informed decision-making. </p>
+            <p class="col_p">Emphasize data security measures and encryption protocols to protect sensitive customer information. Secure payment gateways and trust badges signal commitment to privacy and safeguard customer trust. </p>
+         </div>
+         <div class="about_col">
+            <h4 class="col_subtitle">high</h4>
+            <h3 class="col_title">quality</h3>
+            <img src="assets/img/layout/about_col_img-2.png" alt="" class="col_img">
+            <p class="col_p">Source products that align with your brand's identity and cater to your target audience's needs. Prioritize exceptional craftsmanship, durability, and aesthetic appeal. Offer a diverse range of products to cater to a wider audience. </p>
+            <p class="col_p">Craft an intuitive and user-friendly website that facilitates effortless navigation. Ensure clear categorization, efficient search functionality, and a consistent design across all devices. Optimize page loading times for a smooth browsing experience.</p>
+         </div>
+         <div class="about_col">
+            <h4 class="col_subtitle">delivery</h4>
+            <h3 class="col_title">speed</h3>
+            <img src="assets/img/layout/about_col_img-3.png" alt="" class="col_img">
+            <p class="col_p">In the fast-paced world of e-commerce, speed is paramount. With customers expecting their orders to arrive quickly, offering fast delivery has become a crucial competitive advantage. Online stores that can consistently deliver orders in a timely manner not only delight customers but also build brand loyalty and encourage repeat business.</p>
+            <p class="col_p">Fast delivery is a major factor influencing customer satisfaction. Studies have shown that a significant portion of online shoppers are willing to pay extra for expedited shipping.</p>
+         </div>
+         <div class="about_col">
+            <h4 class="col_subtitle">low</h4>
+            <h3 class="col_title">price</h3>
+            <img src="assets/img/layout/about_col_img-4.png" alt="" class="col_img">
+            <p class="col_p">Affordability is not just about low prices; it's about providing value for money. Identify the unique benefits and features that your products offer, and communicate them clearly to customers. Highlight the savings they can achieve without sacrificing quality or functionality. </p>
+            <p class="col_p">Offer a mix of affordable and premium products to cater to a broader customer segment. Position your affordable offerings as high-quality, value-driven alternatives to more expensive counterparts. Ensure that all products meet your brand's standards and provide exceptional customer experience.</p>
+         </div>
+      </div>
+   </div>
+</section>
+<hr class="about_hr">
