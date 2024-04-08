6cecac30b3ed31b5cd8c313314fc48bcb3c13fbb
Danil Burlakov <1321_budaan_nap@gagpk.ru>
2024-04-08 20:37:29 +0700

    lil coordination

--- templates/header.php
+++ templates/header.php
@@ -11,22 +11,33 @@
       <div class="right">
          <div class="header_nav">
             <a href="/liked" class="header_link">liked</a>
-            <?
-            if (isset($_SESSION['user'])) {
-            ?>
+            <? if (isset($_SESSION['user'])) { ?>
                <a href="/account" class="header_link"><?= $_SESSION['user'] ?></a>
                <a href="/cart" class="header_link">cart</a>
-            <?
-            } else {
-            ?>
+            <? } else { ?>
                <a href="/login" class="header_link">account</a>
-            <?
-            }
-            ?>
+            <? } ?>
          </div>
+      </div>
+      <div class="header_burger">
+         <span class="burger_bar"></span>
+         <span class="burger_bar"></span>
+         <span class="burger_bar"></span>
       </div>
    </div>
 </header>
+<ul class="header_mobile_nav">
+   <a href="/catalog" class="header_link">catalog</a>
+   <a href="/sales" class="header_link">sales</a>
+   <a href="/liked" class="header_link">liked</a>
+   <? if (isset($_SESSION['user'])) { ?>
+      <a href="/account" class="header_link"><?= $_SESSION['user'] ?></a>
+      <a href="/cart" class="header_link">cart</a>
+   <? } else { ?>
+      <a href="/login" class="header_link">account</a>
+   <? } ?>
+   <input type="text" class="header_link search" placeholder="Search">
+</ul>
 <div class="container">
    <div class="search_content">Type something...</div>
 </div>
